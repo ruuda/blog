1f6633e4e4647680c9c075b72bdb57564434bda7
Ruud van Asseldonk <dev@veniogames.com>
2015-10-15 21:52:47 +0100

    Add template interpreter
    There still is a bug, but expansions of variables works. It is amazing
    that a simple templating engine that supports variable expansion, loops,
    and conditionals, can be written in about 70 lines of Haskell!

--- Template.hs
+++ Template.hs
@@ -1,6 +1,11 @@
+import           Control.Monad (join)
+import qualified Data.Map as M
+import           Data.Maybe (fromMaybe)
+
 data Token = Outer String | Inner String
   deriving (Show) -- TODO: This is for debugging only, remove.
 
+-- Splits a string into things inside and outside of moustaches.
 tokenize :: String -> [Token]
 tokenize str = next str (Outer "") []
   where next :: String -> Token -> [Token] -> [Token]
@@ -17,13 +22,51 @@ data Fragment = Loop String
               | Raw String
   deriving (Show) -- TODO This is for debugging only, remove.
 
-fragmentize :: Token -> Fragment
-fragmentize (Outer str) = Raw str
-fragmentize (Inner str) = case span (/= ' ') str of
-  ("foreach ", list) -> Loop list
-  ("if ", condition) -> Conditional condition
-  ("end", "")        -> End
-  (variable, "")     -> Variable variable
-
+-- Converts a string into fragments that can be fed into the interpreter.
 parse :: String -> [Fragment]
-parse = fmap fragmentize . tokenize
+parse = fmap toFragment . tokenize
+  where toFragment (Outer str) = Raw str
+        toFragment (Inner str) = case span (/= ' ') str of
+          ("foreach ", list) -> Loop list
+          ("if ", condition) -> Conditional condition
+          ("end", "")        -> End
+          (variable, "")     -> Variable variable
+
+data Context = StringContext String
+             | MapContext (M.Map String Context)
+             | ListContext [Context]
+
+-- Applies the template (fragments) with given context until an `End` is
+-- encountered, at which point the currint string and the remaining fragments
+-- are returned.
+applyBlock :: [Fragment] -> Context -> (String, [Fragment])
+applyBlock fragments context = next fragments ""
+  where lookup name = case context of
+          MapContext map -> M.lookup name map
+          _              -> Nothing
+        expand variable = case lookup variable of
+          Just (StringContext str) -> str
+          Just (ListContext _)     -> variable ++ " is a list"
+          Just (MapContext _)      -> variable ++ " is a map"
+          Nothing                  -> "undefined"
+        isTrue condition = case lookup condition of
+          Just (StringContext "false") -> False
+          Just _                       -> True -- TODO: should an empty list or empty map be false?
+          Nothing                      -> False
+        getList name = case lookup name of
+          Just (ListContext list) -> list
+          _                       -> []
+        next :: [Fragment] -> String -> (String, [Fragment])
+        next              [] str = (str, [])
+        next (fragment:more) str = case fragment of
+          End               -> (str, more)
+          Raw contents      -> next more (str ++ contents)
+          Variable variable -> next more (str ++ expand variable)
+          Conditional cond  -> next continue $ if (isTrue cond) then str ++ inner else str
+            where (inner, continue) = applyBlock more context
+          Loop list         -> next continue $ str ++ inner
+            where inner         = join $ fmap (\ctx -> fst $ applyBlock more ctx) $ getList list
+                  (_, continue) = applyBlock more context
+
+apply :: [Fragment] -> Context -> String
+apply fragments context = fst $ applyBlock fragments context
