61682682723986abba8ffb31cc4e7c9368556078
Ruud van Asseldonk <dev@veniogames.com>
2015-10-17 15:22:42 +0100

    Add function that renders a post
    This takes a markdown string that includes fron matter (a post file,
    basically) and returns a map which consists of the front matter and a
    "body" field that has the rendered post body.

--- Post.hs
+++ Post.hs
@@ -5,6 +5,7 @@
 -- the licence file in the root of the repository.
 
 import qualified Data.Map as M
+import           Text.Pandoc
 
 -- Front matter consists of key value pairs, both of type string.
 -- There is no fancy YAML here.
@@ -19,3 +20,15 @@ extractFrontMatter = parseFM M.empty . drop 1 . lines
         parseFM fm (line:more)  = parseFM (M.insert key value fm) more
           where (key, delimValue) = break (== ':') line
                 value = drop 2 delimValue -- Drop the colon and space.
+
+-- A post is its front matter, plus the "body" field set to rendered html.
+type Post = M.Map String String
+
+renderPost :: String -> Post
+renderPost str = M.insert "body" html fm
+  where (fm, bodymd) = extractFrontMatter str
+        ropt         = def -- TODO: set correct reader options
+        wopt         = def -- TODO: set correct writer options
+        html         = case fmap (writeHtmlString wopt) (readCommonMark ropt bodymd) of
+          Right result -> result
+          Left _       -> "failed to parse markdown"
