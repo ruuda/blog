153fba65cc7fbe5e67e9bd1d2c8147853ebf6f46
Ruud van Asseldonk <dev@veniogames.com>
2015-10-17 22:44:57 +0100

    Add IO functions to deal with templates and posts

---
+++ Main.hs
@@ -0,0 +1,42 @@
+-- Copyright 2015 Ruud van Asseldonk
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License version 3. See
+-- the licence file in the root of the repository.
+
+import           Control.Monad (filterM, mapM)
+import qualified Data.Map as M
+import           System.Directory (doesFileExist, getDirectoryContents)
+import           System.FilePath (takeBaseName)
+
+import qualified Post as P
+import qualified Template as T
+
+-- Applies the IO-performing function f to every file in a given directory.
+mapFiles :: (FilePath -> IO a) -> FilePath -> IO [a]
+mapFiles f dir = getDirectoryContents dir >>= filterM doesFileExist >>= mapM f
+
+-- Applies the IO-performing function f to every file in a given directory, and
+-- returns a map from the file basename to the result.
+mapFilesBaseName :: (FilePath -> IO a) -> FilePath -> IO (M.Map String a)
+mapFilesBaseName f = (fmap M.fromList) . (mapFiles makePair)
+  where makePair fname = fmap (\x -> (takeBaseName fname, x)) (f fname)
+
+-- Reads and parses all templates in the given directory.
+readTemplates :: FilePath -> IO (M.Map String T.Template)
+readTemplates = mapFilesBaseName $ (fmap T.parse) . readFile
+
+-- Reads a post from a file.
+readPost :: FilePath -> IO P.Post
+readPost fname = fmap makePost $ readFile fname
+  where makePost body = P.renderPost (takeBaseName fname) body
+
+-- Reads and renders all posts in the given directory.
+readPosts :: FilePath -> IO [P.Post]
+readPosts = mapFiles readPost
+
+main :: IO ()
+main = do
+  templates <- readTemplates "templates/"
+  posts     <- readPosts     "posts/"
+  putStrLn "TODO: actually generate something"
