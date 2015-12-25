afae82b9c5f2dd296d4b63732a9fabcbe8f0f527
Ruud van Asseldonk <dev@veniogames.com>
2015-12-25 12:21:24 +0100

    Add script to generate baseline grid
    This script adds a baseline grid to the page to verify baseline
    alignment of the elements on the page.

---
+++ grid.js
@@ -0,0 +1,28 @@
+// Paste this into a js console to add a baseline grid to the page.
+
+var container = document.createElement('div');
+container.style.position = 'absolute';
+container.style.top      = '0px';
+container.style.left     = '0px';
+container.style.width    = '100%';
+
+var height = document.body.getClientRects()[0].height;
+document.body.insertBefore(container, document.body.firstChild);
+
+var i = 0;
+var h = 0;
+while (h < height)
+{
+  var block = document.createElement('div');
+  block.style.backgroundColor = 'cyan';
+  block.style.opacity  = 0.2;
+  block.style.position = 'absolute';
+  block.style.top      = (i * 1.4) + 'em';
+  block.style.left     = '0px';
+  block.style.width    = '100%';
+  block.style.height   = '1.4em';
+  
+  container.appendChild(block);
+  i += 2;
+  h += 2 * block.getClientRects()[0].height;
+}
