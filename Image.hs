42ee33e24b4a4021bbbfd639c0c3d0a55977a8aa
Ruud van Asseldonk <dev@veniogames.com>
2016-01-02 23:00:50 +0000

    Add function to add base64 placeholder to images
    This reads a png file from the placeholder directory, encodes that as a
    data url, and attaches it as a background image to <img> tags via an
    inline style attribute.

--- Image.hs
+++ Image.hs
@@ -9,14 +9,21 @@ module Image (processImages) where
 import           Codec.Picture.Types (dynamicMap)
 import           Codec.Picture (DynamicImage(..), imageWidth, imageHeight, readImage)
 import           Control.Monad ((>=>))
+import qualified Data.ByteString.Char8 as BSC
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Base64 as Base64
 import           Data.List (find)
 import           Data.Maybe (fromJust)
-import           System.FilePath ((</>), takeFileName)
+import           System.FilePath ((</>), (<.>), takeBaseName, takeFileName)
 import qualified Text.HTML.TagSoup as S
 
 import qualified Html
 
 type Attributes = [(String, String)]
+
+-- Returns the value of the "src" attribute.
+getSrc :: Attributes -> String
+getSrc = snd . fromJust . find ((== "src") . fst)
 
 makeDimensions :: DynamicImage -> Attributes
 makeDimensions img = [ ("width",  show $ dynamicMap imageWidth  img)
@@ -26,8 +33,7 @@ makeDimensions img = [ ("width",  show $ dynamicMap imageWidth  img)
 -- adds width and height attributes.
 addDimensions :: FilePath -> Attributes -> IO Attributes
 addDimensions imgDir attrs = fmap (attrs ++) dimensions
-  where src        = snd $ fromJust $ find ((== "src") . fst) attrs
-        imgPath    = imgDir </> (takeFileName src)
+  where imgPath    = imgDir </> (takeFileName $ getSrc attrs)
         dimensions = fmap (either error makeDimensions) (readImage imgPath)
 
 -- Maps an IO-performing function over the attributes of all <img> tags.
@@ -40,8 +46,27 @@ mapImgAttributes f = mapM mapTag
 addDimensionsAll :: FilePath -> [Html.Tag] -> IO [Html.Tag]
 addDimensionsAll imgDir = mapImgAttributes $ addDimensions imgDir
 
+-- Returns file contents as a base64-encoded string.
+readFileBase64 :: FilePath -> IO String
+readFileBase64 = (fmap $ BSC.unpack . Base64.encode) . BS.readFile
+
+-- Wraps a base64 string in a data url.
+makePngDataUrl :: String -> String
+makePngDataUrl base64 = "data:image/png;base64," ++ base64
+
+-- Wraps base64 image contents in an inline style attribute.
+makePlaceholderStyle :: String -> String
+makePlaceholderStyle base64 = "background-image:url(" ++ (makePngDataUrl base64) ++ ")"
+
+addPlaceholder :: FilePath -> Attributes -> IO Attributes
+addPlaceholder placeholderDir attrs = fmap (:attrs) placeholder
+  where imgPath     = placeholderDir </> (takeBaseName $ getSrc attrs) <.> "png"
+        imgBase64   = readFileBase64 imgPath
+        style       = fmap makePlaceholderStyle imgBase64
+        placeholder = fmap (\s -> ("style", s)) style
+
 addPlaceholdersAll :: FilePath -> [Html.Tag] -> IO [Html.Tag]
-addPlaceholdersAll placeholderDir html = return html
+addPlaceholdersAll placeholderDir = mapImgAttributes $ addPlaceholder placeholderDir
 
 -- Given a piece of html, inserts a placeholder background for every <img> tag,
 -- and adds the image dimensions to the attributes of the tag.
