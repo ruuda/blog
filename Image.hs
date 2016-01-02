523d1352bab06ccb42add7e0ece784ea24ef2b49
Ruud van Asseldonk <dev@veniogames.com>
2016-01-02 21:54:20 +0000

    Add function that adds dimensions to <img> tags
    It scans the html for <img> tags and for every tag it opens the image
    from the given path, extracts the dimensions, and adds them to the tag
    as attributes. No more content jumping around because the browser
    suddenly loaded an image!

--- Image.hs
+++ Image.hs
@@ -6,20 +6,46 @@
 
 module Image (processImages) where
 
-import           Control.Monad ((>=>), liftM)
+import           Codec.Picture.Types (dynamicMap)
+import           Codec.Picture (DynamicImage(..), imageWidth, imageHeight, readImage)
+import           Control.Monad ((>=>))
+import           Data.List (find)
+import           Data.Maybe (fromJust)
+import           System.FilePath ((</>), takeFileName)
+import qualified Text.HTML.TagSoup as S
 
 import qualified Html
 
--- Sets the width and height attributes of all <img> tags.
-addDimensions :: FilePath -> [Html.Tag] -> IO [Html.Tag]
-addDimensions imgDir html = error "not implemented"
+type Attributes = [(String, String)]
 
-addPlaceholders :: FilePath -> [Html.Tag] -> IO [Html.Tag]
-addPlaceholders placeholderDir html = error "not implemented"
+makeDimensions :: DynamicImage -> Attributes
+makeDimensions img = [ ("width",  show $ dynamicMap imageWidth  img)
+                     , ("height", show $ dynamicMap imageHeight img) ]
+
+-- Given the attributes of an <img> tag, which must include the src attribute,
+-- adds width and height attributes.
+addDimensions :: FilePath -> Attributes -> IO Attributes
+addDimensions imgDir attrs = fmap (attrs ++) dimensions
+  where src        = snd $ fromJust $ find ((== "src") . fst) attrs
+        imgPath    = imgDir </> (takeFileName src)
+        dimensions = fmap (either error makeDimensions) (readImage imgPath)
+
+-- Maps an IO-performing function over the attributes of all <img> tags.
+mapImgAttributes :: (Attributes -> IO Attributes) -> [Html.Tag] -> IO [Html.Tag]
+mapImgAttributes f = mapM mapTag
+  where mapTag (S.TagOpen "img" attrs) = fmap (S.TagOpen "img") $ f attrs
+        mapTag otherTag                = return otherTag
+
+-- Sets the width and height attributes of all <img> tags.
+addDimensionsAll :: FilePath -> [Html.Tag] -> IO [Html.Tag]
+addDimensionsAll imgDir = mapImgAttributes $ addDimensions imgDir
+
+addPlaceholdersAll :: FilePath -> [Html.Tag] -> IO [Html.Tag]
+addPlaceholdersAll placeholderDir html = return html
 
 -- Given a piece of html, inserts a placeholder background for every <img> tag,
 -- and adds the image dimensions to the attributes of the tag.
 processImages :: FilePath -> FilePath -> String -> IO String
-processImages imgDir placeholderDir = (liftM Html.renderTags)
-  . (addDimensions imgDir >=> addPlaceholders placeholderDir)
+processImages imgDir placeholderDir = (fmap Html.renderTags)
+  . (addDimensionsAll imgDir >=> addPlaceholdersAll placeholderDir)
   . Html.parseTags
