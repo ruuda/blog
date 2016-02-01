f71ff7aeff5b03a14dd27b863b7529a2b62da0c6
Ruud van Asseldonk <dev@veniogames.com>
2016-02-01 17:24:54 +0100

    Do not include monospace font on contact page
    The monospace font is not used there, no need to include the css.

--- src/Main.hs
+++ src/Main.hs
@@ -129,7 +129,6 @@ writeArchive globalContext template posts = writePage 1 "/writing" context templ
 writeContact :: Template.Context -> Template.Template -> Config -> IO Artifact
 writeContact globalContext = writePage 2 "/contact" context
   where context = M.unions [ Template.stringField "title"     "Contact Ruud van Asseldonk"
-                           , Template.stringField "mono-font" "true"
                            , Template.stringField "light"     "true"
                            , globalContext ]
 
